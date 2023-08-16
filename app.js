62227c48f557464826ea4846a589363a49438257
Bjørn Hansen <55191733+bh1995@users.noreply.github.com>
2023-08-16 12:38:33 +0000

    functioning web app for recipe recommendations.

---
+++ app.js
@@ -0,0 +1,30 @@
+const form = document.getElementById('ingredient-form');
+const resultsDiv = document.getElementById('recipe-results');
+
+form.addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+    const ingredients = document.getElementById('ingredients').value;
+    const apiKey = 'e20378718dfd444d98d7c4f3f10318d3';
+    const url = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=${ingredients}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (data.length > 0) {
+            const recipes = data.map(recipe => `
+                <div class="recipe">
+                    <h2>${recipe.title}</h2>
+                    <img src="${recipe.image}" alt="${recipe.title}">
+                </div>
+            `).join('');
+
+            resultsDiv.innerHTML = recipes;
+        } else {
+            resultsDiv.innerHTML = 'No recipes found for the given ingredients.';
+        }
+    } catch (error) {
+        console.error('Error fetching recipes:', error);
+    }
+});
