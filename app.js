8544760595466a369d0ee05531f6796bbf61937f
Bjørn Hansen <55191733+bh1995@users.noreply.github.com>
2023-08-17 09:24:38 +0000

    Functioning app

--- app.js
+++ app.js
@@ -1,32 +1,57 @@
-const form = document.getElementById('ingredient-form');
-const resultsDiv = document.getElementById('recipe-results');
+// Get references to the input elements and the container for recipe cards
+const ingredientInput = document.getElementById('ingredient-input');
+const getRecipesButton = document.getElementById('get-recipes-button');
+const recipesContainer = document.getElementById('recipes-container');
 
-form.addEventListener('submit', async function (event) {
-    event.preventDefault();
+// Base URL for the Spoonacular API
+const spoonacularBaseUrl = 'https://api.spoonacular.com/recipes/findByIngredients';
 
-    const ingredients = document.getElementById('ingredients').value;
-    // const apiKey = 'e20378718dfd444d98d7c4f3f10318d3';
-    // require('dotenv').config();
-    const apiKey = process.env['SPOONACULAR_API_KEY'];
-    const url = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=${ingredients}`;
+// Your Spoonacular API key
+// const SPOONACULAR_API_KEY = process.env.SPOONACULAR_API_KEY;
+// console.log("api key", SPOONACULAR_API_KEY);
+const SPOONACULAR_API_KEY = 'e20378718dfd444d98d7c4f3f10318d3';
 
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
+// Function to fetch recipes and display them as recipe cards
+async function getRecipesAndDisplay() {
+  // Clear previous recipe cards
+  recipesContainer.innerHTML = '';
 
-        if (data.length > 0) {
-            const recipes = data.map(recipe => `
-                <div class="recipe">
-                    <h2>${recipe.title}</h2>
-                    <img src="${recipe.image}" alt="${recipe.title}">
-                </div>
-            `).join('');
+  // Get user-entered ingredients
+  const ingredients = ingredientInput.value.split(',').map(ingredient => ingredient.trim());
 
-            resultsDiv.innerHTML = recipes;
-        } else {
-            resultsDiv.innerHTML = 'No recipes found for the given ingredients.';
-        }
-    } catch (error) {
-        console.error('Error fetching recipes:', error);
-    }
-});
+  // Create the URL for the Spoonacular API request
+  const apiUrl = `${spoonacularBaseUrl}?apiKey=${SPOONACULAR_API_KEY}&ingredients=${ingredients.join(',')}`;
+
+  try {
+    // Fetch data from the Spoonacular API
+    const response = await fetch(apiUrl);
+    const recipes = await response.json();
+
+    // Iterate through the recipes and create recipe cards
+    recipes.forEach(recipe => {
+      // console.log("recipe response", recipe)
+      // Create a recipe card container
+      const recipeCard = document.createElement('div');
+      recipeCard.classList.add('recipe-card');
+
+      // Create an image element
+      const recipeImage = document.createElement('img');
+      recipeImage.src = recipe.image;
+      recipeImage.alt = recipe.title;
+      recipeCard.appendChild(recipeImage);
+
+      // Create a title element
+      const recipeTitle = document.createElement('h2');
+      recipeTitle.textContent = recipe.title;
+      recipeCard.appendChild(recipeTitle);
+
+      // Append the recipe card to the container
+      recipesContainer.appendChild(recipeCard);
+    });
+  } catch (error) {
+    console.error('Error fetching recipes:', error);
+  }
+}
+
+// Add event listener to the "Get Recipes" button
+getRecipesButton.addEventListener('click', getRecipesAndDisplay);
